01f608356d0970e36b39adea1ba71e51fad72168
Nishith Nishith <lnu.nish@northeastern.edu>
2023-11-29 06:58:15 -0500

    lambda updates

--- serv/index.mjs
+++ serv/index.mjs
@@ -1,8 +1,11 @@
 import { DynamoDB, DynamoDBClient  } from '@aws-sdk/client-dynamodb';
 import {  PutCommand, DynamoDBDocument, DynamoDBDocumentClient  } from '@aws-sdk/lib-dynamodb';
 import { Storage } from '@google-cloud/storage';
-import shell from 'shelljs';
-import simpleGit from 'simple-git';
+// import formData from 'form-data';
+// import Mailgun from 'mailgun.js';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
 const dynamo = DynamoDBDocument.from(new DynamoDB());
 const client = new DynamoDBClient({});
@@ -11,50 +14,85 @@ const docClient = DynamoDBDocumentClient.from(client);
 let dbName = process.env.dbName
 let gcpBucket = process.env.gcpBucket
 let privateKey = process.env.privateKey
+let mailKey = process.env.mailKey
 
 let base64PrivateKey = privateKey;
 const text = Buffer.from(base64PrivateKey, 'base64').toString('utf-8');
 
-// console.log('key ',text)
 
 const storage = new Storage({
     credentials: JSON.parse(text),
 });
 
 export const handler = async (event) => {
-  const url = event.Records[0].Sns.Message;
+  const msg = event.Records[0].Sns.Message;
+  let [email, url] = msg.split(',')
   console.log('From SNS:', url);
     
   //download the code
-  // const url = "https://github.com/NishithKody/Neetcode.git"
-  const destination = '/tmp'
 
-  // shell.exec('cd /tmp')
-  // shell.exec(`mkdir test`)
-
-  // simpleGit().clone(url, destination, (error, result) => {
-  //   if (error) {
-  //     console.error('Error cloning repository:', error);
-  //   } else {
-  //     console.log('Repository cloned successfully:', result);
-  //   }
-  // });
+  // const fileUrl = 'https://github.com/NishithNishith/INFO6150/archive/refs/heads/main.zip';
+  const fileUrl = url;
+  const fileName = path.join('/tmp', 'myfile.zip');
+  
+  const file = fs.createWriteStream(fileName);
+  
+  let mailMsg = "Your assignment was submitted successfully"
+  let status = 1;
+  
+  try{
+    https.get(fileUrl, response => {
+    response.pipe(file);
+  
+    file.on('finish', () => {
+      file.close();
+    });
+    }).on('error', error => {
+      console.log('error while downloading the file');
+      status = 0;
+      fs.unlink(fileName);
+    });
+    console.log('the file was downloaded')
+  }
+  catch(err){
+    console.log('error while downloading the file2');
+    status = 0;
+  }
+  
+  
+  console.log('status',status)
+  
   
   //save in google cloud storage
   
-  const bucket = storage.bucket(gcpBucket);
+  // const bucket = storage.bucket(gcpBucket);
 
-  await bucket.upload('./index.mjs', {
-      destination: './test',
-  });
+  // await bucket.upload('./index.mjs', {
+  //     destination: './test',
+  // });
   
   //send email
+
+  // const mailgun = new Mailgun(formData);
+  // const client = mailgun.client({username: 'api', key: mailKey});
+
+  // client.messages.create('sandbox-123.mailgun.org', {
+  //   from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+  //   to: ["nishith0514@gmail.com"],
+  //   subject: "Hello",
+  //   text: "Testing some Mailgun awesomeness!",
+  //   html: "<h1>Testing some Mailgun awesomeness!</h1>"
+  // })
+  // .then(msg => console.log(msg)) // logs response data
+  // .catch(err => console.log(err)); // logs any error
+
   
   //save the resp in db
   const command = new PutCommand({
     TableName: dbName,
     Item: {
-      key: "Shiba Inu",
+      email: email,
+      submission: url
     },
   });
   const response = await docClient.send(command);
